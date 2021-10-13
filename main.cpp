02caecc3537e27ef9c95acc2dadb9ea9c8383d0b
MU15 <>
2021-10-13 14:14:02 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+int lustro=5;
+int segundos=0;
+int casa=0; 
+segundos=lustro*365*24*60*60;
+cout<< "en nuestro hay"<<segundos<< "segundos";
+casa =casa+1;
+
+}
